0b363575019982610ce3eb5fb0847791bc014e86
Shishir420-GIT <shishirsrivastav@gmail.com>
2024-04-16 22:15:14 +0530

    Completed: uploading a post to DB.

---
+++ src/lib/appwrite/api.ts
@@ -0,0 +1,205 @@
+import { ID, Query } from "appwrite";
+import { INewPost, INewUser } from "@/types";
+import { appwriteConfig, account, databases, storage, avatars } from "./config";
+
+export async function createUserAccount(user: INewUser) {
+    try {
+        const newAccount = await account.create(
+            ID.unique(),
+            user.email,
+            user.password,
+            user.name
+        );
+        if(!newAccount) throw Error;
+
+        const avatarurl = avatars.getInitials(user.name);
+        
+        const newUser = await saveUserToDB({
+            accountId: newAccount.$id,
+            name: newAccount.name,
+            email: newAccount.email,
+            username: user.username,
+            imageUrl: avatarurl,
+        });
+        //console.log(newUser.name)
+        //console.log("newUser is: ")
+        //console.log(newUser)
+        return newUser;
+    }
+    catch (error){
+        console.log(error);
+        return error;
+    }
+}
+
+export async function saveUserToDB(user: {
+    accountId: string;
+    email: string;
+    name: string;
+    imageUrl: URL;
+    username?: string;
+}) {
+    try {
+        const newUser = await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            ID.unique(),
+            user
+        );
+        //console.log("saved to db is: ")
+        //console.log(newUser)
+        return newUser
+    }
+    catch(error){
+        console.log(error)
+        return null;
+    }
+}
+
+export async function signInAccount(user: { email: string; password: string }) {
+    try {
+      //console.log("Creating email sesssion")
+      const session = await account.createEmailSession(user.email, user.password);
+      //console.log("Created email sesssion")
+      return session;
+    } catch (error) {
+        //console.log("Error in sesssion")
+        console.log(error);
+    }
+  }
+
+export async function getAccount() {
+    try {
+      //console.log("Getting account")
+      const currentAccount = await account.get();
+      //console.log("got account")
+      //console.log(currentAccount)
+      return currentAccount;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+export async function getCurrentUser(){
+    try{
+        //console.log("Getting current user")
+        const currentAccount = await getAccount();
+
+        if(!currentAccount) throw Error;
+        //console.log("listing current user")
+        const currentUser = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            [Query.equal("accountId", currentAccount.$id)]
+        );
+        //console.log("got current user")
+        //console.log(currentUser)
+        if(!currentUser) throw Error;
+
+        return currentUser.documents[0];
+    }
+    catch(error){
+        console.log(error)
+        return null;
+    }
+}
+
+export async function signOutAccount() {
+    try{
+        const session = await account.deleteSession("current")
+        return session
+    }
+    catch(error){
+        console.log(error)
+    }
+    
+}
+
+// ============================== UPLOAD FILE
+export async function uploadFile(file: File) {
+    try {
+      const uploadedFile = await storage.createFile(
+        appwriteConfig.storageId,
+        ID.unique(),
+        file
+      );
+  
+      return uploadedFile;
+    } catch (error) {
+      console.log(error);
+    }
+}
+  
+// ============================== GET FILE URL
+export function getFilePreview(fileId: string) {
+    try {
+        const fileUrl = storage.getFilePreview(
+        appwriteConfig.storageId,
+        fileId,
+        2000,
+        2000,
+        "top",
+        100
+        );
+
+        if (!fileUrl) throw Error;
+
+        return fileUrl;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// ============================== DELETE FILE
+export async function deleteFile(fileId: string) {
+    try {
+      await storage.deleteFile(appwriteConfig.storageId, fileId);
+  
+      return { status: "ok" };
+    } catch (error) {
+      console.log(error);
+    }
+}
+
+export async function createPost(post: INewPost) {
+    try {
+      // Upload file to appwrite storage
+      const uploadedFile = await uploadFile(post.file[0]);
+  
+      if (!uploadedFile) throw Error;
+  
+      // Get file url
+      const fileUrl = getFilePreview(uploadedFile.$id);
+      if (!fileUrl) {
+        await deleteFile(uploadedFile.$id);
+        throw Error;
+      }
+  
+      // Convert tags into array
+      const tags = post.tags?.replace(/ /g, "").split(",") || [];
+  
+      // Create post
+      const newPost = await databases.createDocument(
+        appwriteConfig.databaseId,
+        appwriteConfig.postCollectionId,
+        ID.unique(),
+        {
+          creator: post.userId,
+          caption: post.caption,
+          imageUrl: fileUrl,
+          imageId: uploadedFile.$id,
+          location: post.location,
+          tags: tags,
+        }
+      );
+  
+      if (!newPost) {
+        await deleteFile(uploadedFile.$id);
+        throw Error;
+      }
+  
+      return newPost;
+    } catch (error) {
+      console.log(error);
+    }
+  }
---
+++ src/lib/validation/index.ts
@@ -0,0 +1,21 @@
+import * as z from "zod"
+
+// 1. Define your schema.
+export const SignupValidation = z.object({
+    name: z.string().min(2, { message: "Too short"}),
+    username: z.string().min(2).max(50),
+    email: z.string().email({ message: "Invalid email"}),
+    password: z.string().min(8, { message: "Password must be 8 characters"})
+})
+
+export const SigninValidation = z.object({
+    email: z.string().email({ message: "Invalid email"}),
+    password: z.string().min(8, { message: "Password must be 8 characters"})
+})
+
+export const PostValidation = z.object({
+    caption: z.string().min(2).max(2048),
+    location: z.string().min(6).max(1024),
+    tags: z.string(),
+    file: z.custom<File>(),
+})
